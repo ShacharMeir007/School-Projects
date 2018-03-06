a5cdcb2d34c9bdb87960bded08ea6041c72841b6
shach <shachar.meir007@gmail.com>
2018-03-06 23:27:45 +0200

    6/3 shachar / php include menu.html

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
+<?php
+    include_once 'dbh.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Gallery</title>
+    <title>login</title>
 </head>
 <body>
-<form method="Get">
 
-    <input type="text" name="num1">
-    <button> submit</button>
-</form>
-
-<?php
-$fine = $_GET['person'];
-$r = array("f","fd");
-echo $fine;
-echo $r['0'];
-if($fine == $r){}
-
-?>
 
 </body>
 </html>
