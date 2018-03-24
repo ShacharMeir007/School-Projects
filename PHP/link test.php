99d7eed408313256d56fd8b589466f67e2841f02
shach <shachar.meir007@gmail.com>
2018-03-24 20:27:11 +0300

    shachar sign up system complete

---
+++ PHP/link test.php
@@ -0,0 +1,10 @@
+<?php
+
+  //this script is for linking tests purposes only
+if (isset($_POST['submit'])) {
+    echo "success";
+}/**/
+else{
+    header("Location: ../PHP/Sign%20up.html");
+    exit();
+}
--- PHP/signup.inc.php
+++ PHP/signup.inc.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "hi";
 if (isset($_POST['submit'])){
     include_once 'dbh.inc.php';
     $first = mysqli_real_escape_string($conn,$_POST['first']);
@@ -10,45 +10,55 @@ if (isset($_POST['submit'])){
     // error handlers
     //check for empty field
     if (empty($first)||empty($last)||empty($email)||empty($uid)||empty($pwd)){
-        header("Location: ../PHP/Sign%20up.php");
+        header("Location: ../PHP/empty%20test.php");
         exit();
-    }else{
+    }else {
         //check if input characters are valid
-        if (!preg_match("/^[a-zA-Z]*$/",$first)|| !preg_match("/^[a-zA-Z]*$/",$last)){
+        if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
             header("Location: ../PHP/Sign%20up.php");
             exit();
-        }
-        else{
+        } else {
             //check if email is valid
-            if (!filter_var($email,FILTER_VALIDATE_EMAIL )){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 header("Location: ../PHP/Sign%20up.php");
                 exit();
-            }
-            else{
-                $sql = "SELECT * FROM users where user_uid =$uid;";
-                $result = mysqli_query($conn,$sql);
+            } else {
+                $sql = "SELECT * FROM users where user_uid ='$uid';";
+                $result = mysqli_query($conn, $sql);
                 $resultCheck = mysqli_num_rows($result);
 
-                if ($resultCheck >0){
-                    header("Location: ../PHP/Sign%20up.php");
+                if ($resultCheck > 0) {
+                    header("Location: ../PHP/username%20test.php");
                     exit();
                 }else{
+                    $sql = "SELECT * FROM users where user_email ='$email';";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+
+                    if ($resultCheck > 0) {
+                        header("Location: ../PHP/username%20test.php");
+                        exit();
+                    }
+
+                else {
                     //hashing the password
-                    $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
-                    $sql = "INSERT INTO users(user_id,user_first,user_last,
+                    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+                    $sql = "INSERT INTO users(user_first,user_last,
                     user_email,user_uid,user_pwd) VALUES ('$first','$last',
                     '$email','$uid','$hashedPwd');";
                     mysqli_query($conn, $sql);
-                    header("Location: ../PHP/Sign%20up.php");
+                    header("Location: link%20test.php");
                     exit();
                 }
+            }
             }
         }
     }
 
 
 
-}else{
-    header("Location: ../PHP/Sign%20up.php");
+}
+else{
+    header("Location: ../PHP/Sign%20up.html");
     exit();
 }
